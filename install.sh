76a5f63f6724697047078a10017aa6a5563eee3f
tpo <tpo@amisw.com>
2019-04-09 15:27:41 +0200

    First commit of config

---
+++ install.sh
@@ -0,0 +1,9 @@
+#Hello
+
+cp .bash_tpo ~/
+cp .zshrc ~/
+mkdir -p ~/.vim/colors
+cp monokai.vim ~/.vim/colors/
+cp .vimrc ~/
+
+grep -qxF 'source ~/.bash_tpo' ~/.bash_tpo || echo 'source ~/.bash_tpo' >> ~/.bash_tpo
